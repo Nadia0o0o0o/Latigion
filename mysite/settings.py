0f42dfb4f069c61b6b127ad0e826eb775fa30bf1
SuperR-bit <ricardochairesgarza@gmail.com>
2025-03-12 10:13:09 -0600

    whitenoise

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'comics', #app de lectura
     'sorl.thumbnail',
     'author', #app de autores
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -145,4 +147,6 @@ LOGOUT_REDIRECT_URL = '/'
 
 
 #STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
